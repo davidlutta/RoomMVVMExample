81e9fd321db1bccacd1af9c24e1ee6f3865829a8
David Lutta <davidlutta2010@gmail.com>
2019-05-16 14:32:06 +0300

    Create new activity to add new notes & create menu

---
+++ app/src/main/java/com/davidlutta/architectureexample/UI/Activities/AddNotesActivity.java
@@ -0,0 +1,16 @@
+package com.davidlutta.architectureexample.UI.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.davidlutta.architectureexample.R;
+
+public class AddNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_notes);
+    }
+}
